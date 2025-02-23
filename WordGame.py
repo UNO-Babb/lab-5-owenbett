4d5ed55bc1e2cea05d2156970c5aa411ec1a56bd
owenbett <olbetts@cox.net>
2025-02-23 19:45:57 +0000

    finished ceasarcipher and WordGame

--- WordGame.py
+++ WordGame.py
@@ -4,20 +4,37 @@ import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
+    for ch in word:
+            if letter == ch:
+                return True 
     return False
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
-    return False
+    correctLetter = word[spot]
+    if letter == correctLetter:
+        return True
+    else:
+        return False
 
 def rateGuess(myGuess, word):
     """Rates your guess and returns a word with the following features.
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
+    feedback = ""
 
+
+    for spot in range(5):
+        myLetter = myGuess[spot]
+        if inSpot(myLetter, word, spot) == True:
+            feedback = feedback + myLetter.upper() #correct letter in location
+        elif inWord(myLetter, word) == True:
+            feedback = feedback + myLetter.lower() #letter is in word, not correct spot
+        else:
+            feedback = feedback + "*"
+
+    return feedback
 
 def main():
     #Pick a random word from the list of all words
@@ -28,8 +45,31 @@ def main():
     print(todayWord)
 
     #User should get 6 guesses to guess
-
+    guessNum = 1
+    while guessNum == 6:
     #Ask user for their guess
+        validWord = False
+        while validWord == False:
+            guess = input("enter Guess: ")
+            guess = guess.lower()
+            if guess not in wordList:
+                print("Word not in list.")
+                validWord = False
+            else:
+                validWord = True
+
+    #give feedback using on their word
+    feedback = rateGuess(guess, todayWord)
+    print(feedback)
+    if feedback == todayWord.upper():
+        print("You got it in", guessNum, "tries!")
+        
+    guessNum = guessNum + 1
+
+    print("the word was", todayWord)
+    print("Bye")
+
+
     #Give feedback using on their word:
 
 
